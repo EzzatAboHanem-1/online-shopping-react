f5f928cb9ba894c8cd6d929ce078a8c6af6d6f1a
Ezzat Abo Hanem <ezzatabohanem9@gmail.com>
2026-02-08 00:14:01 +0200

    Add Logout component

---
+++ src/component/Logout/Logout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+export default function Logout() {
+  return (
+    <div>
+      <Helmet>
+        <title>Logout</title>
+      </Helmet>
+      Logout
+      </div>
+  )
+}
+

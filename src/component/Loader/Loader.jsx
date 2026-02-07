0ada861cb21162952cf85f16005c659d6e67b659
Ezzat Abo Hanem <ezzatabohanem9@gmail.com>
2026-02-08 00:13:00 +0200

    Add Loader component

---
+++ src/component/Loader/Loader.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ThreeCircles } from 'react-loader-spinner';
+
+export default function Loader() {
+  return (
+    <div className='vh-100 d-flex justify-content-center align-items-center'>
+      <ThreeCircles
+  visible={true}
+  height="100"
+  width="100"
+  color="#f4bc4eff"
+  ariaLabel="three-circles-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  />
+    </div>
+  )
+}
+
